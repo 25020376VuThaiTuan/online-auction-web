e70bc62a77f7b740ef5a9cd72fe1e4652de694f1
Son1701 <25020347@vnu.edu.vn>
2026-04-22 21:34:32 +0700

    Update: More updates to AuctionSession

--- src/main/java/org/example/dao/UserDAO.java
+++ src/main/java/org/example/dao/UserDAO.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import model.User;
+import org.example.model.User;
 
 public class UserDAO {
     private Connection conn;
