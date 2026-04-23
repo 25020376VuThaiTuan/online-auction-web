40c0e51c8d694ada71308d4849b4a7a7f13cf36b
Son1701 <25020347@vnu.edu.vn>
2026-04-23 22:13:11 +0700

    Update: Changes in UserNotFound

--- src/main/java/org/example/exception/UserNotFound.java
+++ src/main/java/org/example/exception/UserNotFound.java
@@ -2,6 +2,6 @@ package org.example.exception;
 
 public class UserNotFound extends Exception {
     public UserNotFound(String message) {
-        System.out.println("Error!User not found.");;
+        super(message);
     }
 }
