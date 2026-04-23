aa1ffd7be6915185073592cd26aa1ca6283d4f84
Son1701 <25020347@vnu.edu.vn>
2026-04-23 22:12:12 +0700

    Update: Changes in Bid

--- src/main/java/org/example/model/Bid.java
+++ src/main/java/org/example/model/Bid.java
@@ -1,19 +1,34 @@
 package org.example.model;
+
 import java.time.LocalDateTime;
 
 public class Bid extends Entity {
-    private String bidderId;
-    private String itemId;
-    private double amount;
+    private final String bidderId;
+    private final String itemId;
+    private final double amount;
+    private final LocalDateTime bidTime;
 
     public Bid(String id, String bidderId, String itemId, double amount, LocalDateTime bidTime) {
         super(id);
         this.bidderId = bidderId;
+        this.itemId = itemId;
         this.amount = amount;
+        this.bidTime = bidTime;
+    }
+
+    public String getBidderId() {
+        return bidderId;
+    }
+
+    public String getItemId() {
+        return itemId;
     }
 
     public double getAmount() {
         return amount;
     }
 
+    public LocalDateTime getBidTime() {
+        return bidTime;
+    }
 }
