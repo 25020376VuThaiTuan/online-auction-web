7fbdef0b6b87adeefbafac122455b8f3593d99af
Son1701 <25020347@vnu.edu.vn>
2026-04-27 22:42:19 +0700

    Update : Changes in MainApp

--- src/main/java/org/example/app/MainApp.java
+++ src/main/java/org/example/app/MainApp.java
@@ -1,26 +1,15 @@
 package org.example.app;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.App;
 
 public class MainApp extends Application {
-
     @Override
     public void start(Stage stage) throws Exception {
-
-        FXMLLoader fxmlLoader =
-                new FXMLLoader(MainApp.class.getResource("/view/Login.fxml"));
-
-        Scene scene = new Scene(fxmlLoader.load());
-
-        stage.setTitle("Online Auction System");
-        stage.setScene(scene);
-        stage.show();
+        new App().start(stage);
     }
-
     public static void main(String[] args) {
-        launch();
+        App.main(args);
     }
 }
