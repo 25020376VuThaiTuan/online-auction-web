d3955bcba20dec1cd36328d9831b0e8f5dc34c06
Son1701 <25020347@vnu.edu.vn>
2026-05-02 09:06:51 +0700

    Update : Changes in App

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,21 +1,13 @@
 package org.example;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.util.SceneNavigator;
 
 public class App extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        // Lưu ý: Đường dẫn này phải cực chuẩn
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/main_view.fxml"));
-        Parent root = loader.load();
-
-        primaryStage.setTitle("Hệ thống Đấu giá - Tuần 7 Preview");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        SceneNavigator.switchScene(primaryStage, "/view/Login.fxml", "Online Auction System");
     }
 
     public static void main(String[] args) {
